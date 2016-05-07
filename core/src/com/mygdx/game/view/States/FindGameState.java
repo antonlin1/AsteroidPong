316faf648436d8109ab73a36b372da81bce55270
Johan Månsson <mansson.johan@me.com>
2016-05-07 16:39:58 +0200

    quick fix.

--- core/src/com/mygdx/game/view/States/FindGameState.java
+++ core/src/com/mygdx/game/view/States/FindGameState.java
@@ -17,24 +17,27 @@ public class FindGameState extends State {
     private Texture[] circles;
     private Texture text;
     private Texture button;
-    private long time;
+    private long time1, time2, time3;
 
 
     public FindGameState(StateManager stateManager, WifiDirectInterface wifiDirect) {
        super(stateManager, StateManager.STATE_NAME.FIND_GAME_STATE, wifiDirect);
        cancel = new Texture("cancel2.png");
 
-        circles = new Texture[5];
+        circles = new Texture[7];
         circles[0] = new Texture("s1.png");
         circles[1] = new Texture("s2.png");
         circles[2] = new Texture("s3.png");
         circles[3] = new Texture("s4.png");
         circles[4] = new Texture("s5.png");
+        circles[5] = new Texture("s6.png");
+        circles[6] = new Texture("s7.png");
 
         text = new Texture("selecttext.png");
         button = new Texture("buttonREF.png");
 
-        time = TimeUtils.millis();
+        time1 = TimeUtils.millis();
+        time2 = TimeUtils.millis() + 420;
 
 
     }
@@ -58,24 +61,31 @@ public class FindGameState extends State {
 
         spriteBatch.draw(text, (Gdx.graphics.getWidth() / 2) - (text.getWidth() / 2), (Gdx.graphics.getHeight() / 2) - text.getHeight() - 500);
 
-        if(TimeUtils.timeSinceMillis(time) > 0 && TimeUtils.timeSinceMillis(time) < 150) {
+        if(TimeUtils.timeSinceMillis(time1) > 0 && TimeUtils.timeSinceMillis(time1) < 100) {
             spriteBatch.draw(circles[0], (Gdx.graphics.getWidth() / 2) - (circles[0].getWidth() / 2), (Gdx.graphics.getHeight() / 2) - (circles[0].getHeight() / 2));
         }
-        if(TimeUtils.timeSinceMillis(time) > 150 && TimeUtils.timeSinceMillis(time) < 300) {
+        if(TimeUtils.timeSinceMillis(time1) > 100 && TimeUtils.timeSinceMillis(time1) < 230) {
             spriteBatch.draw(circles[1], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
         }
-        if(TimeUtils.timeSinceMillis(time) > 300 && TimeUtils.timeSinceMillis(time) < 450) {
+        if(TimeUtils.timeSinceMillis(time1) > 230 && TimeUtils.timeSinceMillis(time1) < 360) {
             spriteBatch.draw(circles[2], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
         }
-        if(TimeUtils.timeSinceMillis(time) > 450 && TimeUtils.timeSinceMillis(time) < 600) {
+        if(TimeUtils.timeSinceMillis(time1) > 360 && TimeUtils.timeSinceMillis(time1) < 490) {
             spriteBatch.draw(circles[3], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
         }
-        if(TimeUtils.timeSinceMillis(time) > 600 && TimeUtils.timeSinceMillis(time) < 750) {
+        if(TimeUtils.timeSinceMillis(time1) > 490 && TimeUtils.timeSinceMillis(time1) < 530) {
             spriteBatch.draw(circles[4], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
         }
-        if(TimeUtils.timeSinceMillis(time) > 750) {
-            time = TimeUtils.millis();
+        if(TimeUtils.timeSinceMillis(time1) > 530 && TimeUtils.timeSinceMillis(time1) < 680) {
+            spriteBatch.draw(circles[5], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
         }
+        if(TimeUtils.timeSinceMillis(time1) > 680 && TimeUtils.timeSinceMillis(time1) < 840) {
+            spriteBatch.draw(circles[6], (Gdx.graphics.getWidth()/2) - (circles[0].getWidth()/2), (Gdx.graphics.getHeight()/2) - (circles[0].getHeight()/2));
+        }
+        if(TimeUtils.timeSinceMillis(time1) > 840) {
+            time1 = TimeUtils.millis();
+        }
+
 
         Color c = spriteBatch.getColor();
 
@@ -87,7 +97,6 @@ public class FindGameState extends State {
         spriteBatch.end();
 
     }
-
 
 
     public void handleInput() {
