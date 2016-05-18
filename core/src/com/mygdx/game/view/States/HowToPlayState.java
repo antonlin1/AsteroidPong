26decf1640046657def2b8d21b0b2cdc874a8c79
Johan Månsson <mansson.johan@me.com>
2016-05-18 16:42:16 +0200

    added back button texture to HowToPlayState class

--- core/src/com/mygdx/game/view/States/HowToPlayState.java
+++ core/src/com/mygdx/game/view/States/HowToPlayState.java
@@ -15,11 +15,12 @@ import com.mygdx.game.view.MyGdxGame;
  */
 public class HowToPlayState extends com.mygdx.game.view.States.State {
 
-	private Texture cancel;
 	private Texture text;
 	private Texture text2;
 	private Texture[] movingphone;
 	private Texture button;
+	private Texture backButton;
+
 
 
 	private long time;
@@ -31,7 +32,7 @@ public class HowToPlayState extends com.mygdx.game.view.States.State {
 						  PeerHelperInterface peerHelperInterface) {
 		super(game, stateManager, StateManager.STATE_NAME.HOW_TO_PLAY_STATE,
 				wifiDirect, peerHelperInterface);
-		cancel = new Texture("cancel2.png");
+
 		movingphone = new Texture[3];
 		movingphone[0] = new Texture("phone1.png");
 		movingphone[1] = new Texture("phone2.png");
@@ -42,6 +43,7 @@ public class HowToPlayState extends com.mygdx.game.view.States.State {
 		text = new Texture("howtoplaytext.png");
 		text2 = new Texture("waitingtext2.png");
 		button = new Texture("buttonSG.png");
+		backButton = new Texture("buttonBA.png");
 
 		this.isMultiplayer = isMultiplayer;
 	}
@@ -56,7 +58,7 @@ public class HowToPlayState extends com.mygdx.game.view.States.State {
 
 		spriteBatch.begin();
 		Color c = spriteBatch.getColor();
-		spriteBatch.draw(cancel, Gdx.graphics.getWidth() - cancel.getWidth() - 20, 20);
+		//spriteBatch.draw(cancel, Gdx.graphics.getWidth() - cancel.getWidth() - 20, 20);
 
 		if (isMultiplayer) {
 			spriteBatch.draw(text2, (Gdx.graphics.getWidth() / 2) - (text2.getWidth() / 2), 20);
@@ -65,27 +67,30 @@ public class HowToPlayState extends com.mygdx.game.view.States.State {
 			}
 		}
 		spriteBatch.draw(button, (Gdx.graphics.getWidth() / 2) - (button.getWidth() / 2),
-				Gdx.graphics.getHeight() - button.getHeight() - 100);
+				Gdx.graphics.getHeight() - button.getHeight() - 300);
 
 		spriteBatch.setColor(c.r, c.g, c.b, 1f);
+
+		spriteBatch.draw(backButton, (Gdx.graphics.getWidth() / 2) - (backButton.getWidth() / 2), Gdx.graphics.getHeight() - 300 + 20);
+
 
 		float x1 = (Gdx.graphics.getWidth() / 2) - 250;
 		float x2 = (Gdx.graphics.getWidth() / 2) - (text.getWidth() / 2);
 
-		spriteBatch.draw(text, x2, 750);
+		spriteBatch.draw(text, x2, 600);
 
 
 		if (TimeUtils.timeSinceMillis(time) > 0) {
-			spriteBatch.draw(movingphone[0], x1, 300, 500, 500);
+			spriteBatch.draw(movingphone[0], x1, 150, 500, 500);
 		}
 		if (TimeUtils.timeSinceMillis(time) > 600) {
-			spriteBatch.draw(movingphone[1], x1, 300, 500, 500);
+			spriteBatch.draw(movingphone[1], x1, 150, 500, 500);
 		}
 		if (TimeUtils.timeSinceMillis(time) > 900) {
-			spriteBatch.draw(movingphone[0], x1, 300, 500, 500);
+			spriteBatch.draw(movingphone[0], x1, 150, 500, 500);
 		}
 		if (TimeUtils.timeSinceMillis(time) > 1600) {
-			spriteBatch.draw(movingphone[2], x1, 300, 500, 500);
+			spriteBatch.draw(movingphone[2], x1, 150, 500, 500);
 		}
 
 		if (TimeUtils.timeSinceMillis(time) > 1900) {
@@ -103,24 +108,25 @@ public class HowToPlayState extends com.mygdx.game.view.States.State {
 			float x = Gdx.input.getX();
 			float y = Gdx.input.getY();
 
-			float x1 = Gdx.graphics.getWidth() - cancel.getWidth() - 20;
-			float x2 = Gdx.graphics.getWidth() + cancel.getWidth() - 20;
-			float y1 = 20;
-			float y2 = cancel.getHeight() + 20;
+			float x21 = (Gdx.graphics.getWidth() / 2) - (button.getWidth() / 2);
+			float x22 = (Gdx.graphics.getWidth() / 2) + (button.getWidth() / 2);
 
-			if (x > x1 && x < x2 && y > y1 && y < y2) {
-				System.out.println("Exit pressed");
+			float y11 = Gdx.graphics.getHeight()  - 300 + 20;
+			float y12 = Gdx.graphics.getHeight()  - 300 + (20 + backButton.getHeight()) ;
+
+			if (x > x21 && x < x22 && y > y11 && y < y12) {
+				System.out.println("Back pressed");
 				stateManager.pop();
 
 			}
 
-			float x21 = (Gdx.graphics.getWidth() / 2) - (button.getWidth() / 2);
-			float x22 = (Gdx.graphics.getWidth() / 2) + (button.getWidth() / 2);
-			float y21 = Gdx.graphics.getHeight() - button.getHeight() - 100;
-			float y22 = Gdx.graphics.getHeight() + button.getHeight() - 100;
+
+			float y21 = Gdx.graphics.getHeight() - button.getHeight() - 300;
+			float y22 = Gdx.graphics.getHeight() - 300;
+
 
 			if (x > x21 && x < x22 && y > y21 && y < y22 ) {
-
+				System.out.println("Exit pressed");
 				if(isMultiplayer && wifiDirect.isConnected()) {
 
 //					if(wifiDirect.isServer()){
